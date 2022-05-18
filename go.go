f07d00372354fc949267af3fbad8a845868077eb
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-05-18 08:39:53 +0200

    Update go.go

--- go.go
+++ go.go
@@ -30,13 +30,13 @@ func referenceSolution(n int) int {
 
 var _ = Describe("Tests", func() {
      It("Sample tests", func() {
-       Expect(Totient({})).To(Equal({}))
+       Expect(NAME({})).To(Equal({}))
      })
      It("Random tests", func() {
        rand.Seed(time.Now().UTC().UnixNano())
        for _i := 0 ; _i < 100 ; _i++ {
          n := min + rand.Intn(max - min + 1)
-         Expect(Name({})).To(Equal(referenceSolution({})))
+         Expect(Name()).To(Equal(referenceSolution()))
        }
      })
 })
