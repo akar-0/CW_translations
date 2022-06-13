1ebd45f9402fa879c2c904d388a3c0d4c24b1c75
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-06-13 18:35:21 +0200

    Create nasm.asm

---
+++ nasm.asm
@@ -0,0 +1,12 @@
+global NAME
+
+section .text
+
+      ; <--  SIGNATURE  -->
+      ; n := edi
+      ; b := esi
+      ; c := rdx
+perimeter:
+    xor rax,rax     ; rax, the result
+    ret
+;<--    end of NAME -->
