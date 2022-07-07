101bdc0bea20e91158ed39fc0ce3697e6e7489e2
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-07 14:07:05 +0200

    Update go.go

--- go.go
+++ go.go
@@ -57,7 +57,26 @@ var _ = Describe("Tests", func() {
 // https://www.codewars.com/kumite/62954df52325a5000f1ae1a2?sel=6295a69813690400245fe50d
 
 // approxEquals
-// https://www.codewars.com/kumite/62b84a24b16c63004a07503a?sel=62b84a24b16c63004a07503a
+// https://www.codewars.com/kumite/62c6c4129012c30017decaa0?sel=62c6c4129012c30017decaa0
+
+
+func assertApproxEquals(actual, expected, margin float64) (passed bool) {
+  if expected == 0 {
+    passed = math.Abs(actual) <= margin
+  } else {
+    passed = math.Abs((actual - expected) / expected) <= margin
+  }
+  if ! passed {
+    fmt.Printf("Expected value close to %f, but got %f", expected, actual)
+  }
+  return
+}
+
+func dotest(s, f string, expected float64) {
+     Expect(assertApproxEquals(NAME(s,f),expected, 1e-3)).To(BeTrue())
+}
+
+
 // (..., error)
 // https://www.codewars.com/kumite/5beeecefd0253f259e0002c3?sel=5beeecefd0253f259e0002c3
 // https://www.codewars.com/kumite/62bd7db1aced6310e237a3d8?sel=62bd7db1aced6310e237a3d8
