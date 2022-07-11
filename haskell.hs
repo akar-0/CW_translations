0d48e7f292a1efaf831fd280ba8c804b4bfd04b2
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-11 10:13:27 +0200

    Update haskell.hs

--- haskell.hs
+++ haskell.hs
@@ -1,2 +1,38 @@
+module KataSpec (spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+import Kata (NAME)
+import System.Random
+
+refSol :: 
+                          
+spec :: Spec
+spec = do
+  it "Fixed tests" $ do
+    NAME 11 6 1 `shouldBe` 33
+
+  it "Random tests" $ do
+      doRandomTests 100
+      
+doRandomTests n =
+      forAll (choose(0, n)) $ \ w -> do
+      forAll (choose(0, n)) $ \ h -> do
+      forAll (f w h n) $ \ r -> do
+          NAME h w r `shouldBe`  refSol h w r
+          
+          
+-- credit: donaldsebleung
+f :: Integer -> Integer -> Integer -> Gen Integer
+f w h n = do
+  x <- (choose (0, 10) :: Gen Integer)
+  if x > 0
+  then do
+    y <- choose (1, max h w)
+    return (1 + y)
+  else choose (1, n)
+
+
+
 -- https://www.codewars.com/kumite/5b611d5f9756809e7b00001b?sel=5b611f9d146a281bd800001a
 modifyMaxSuccess (const nTest) $ 
