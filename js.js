b8f5b1232fbbc9b3cda9a91e287133fb975e27b9
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-12 17:04:30 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+const assert = chai.assert;
+// Uncomment the following line to disable truncating failure messages for deep equals, do:
+// chai.config.truncateThreshold = 0;
+
+describe("Solution", function() {
+  it("should test for something", function() {
+     assert.strictEqual(1 + 1, 2);
+  });
+});
