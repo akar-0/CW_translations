78b9ac8cf5fbecefead5f927286418a4288dd58b
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-12 17:05:07 +0200

    Update js.js

--- js.js
+++ js.js
@@ -3,8 +3,11 @@ const assert = chai.assert;
 // Uncomment the following line to disable truncating failure messages for deep equals, do:
 // chai.config.truncateThreshold = 0;
 
-describe("Solution", function() {
-  it("should test for something", function() {
+describe("Tests", function() {
+  it("Fixed tests", function() {
+     assert.strictEqual(1 + 1, 2);
+  });
+  it("Random tests", function() {
      assert.strictEqual(1 + 1, 2);
   });
 });
