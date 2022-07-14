e5bcafefc9252d083c35674ddd3d220b865d6828
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-14 10:24:21 +0200

    Create c++.cpp

---
+++ c++.cpp
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kumite/62cfc49cde31a7003e9642bf?sel=62cfc49cde31a7003e9642bf
+
+// https://docs.codewars.com/languages/cpp/igloo/stringizers
+
+// https://docs.codewars.com/languages/cpp/authoring/#custom-assertion-messages
