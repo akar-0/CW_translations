79c01fe2c1c2e513f832987b565cad8521bb5ac2
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-14 18:50:09 +0200

    Update nasm.asm

--- nasm.asm
+++ nasm.asm
@@ -1,11 +1,12 @@
 global NAME
 
+; <--  SIGNATURE  -->
+; n := edi
+; b := esi
+; c := rdx
+      
 section .text
 
-      ; <--  SIGNATURE  -->
-      ; n := edi
-      ; b := esi
-      ; c := rdx
 NAME:
     xor rax,rax     ; rax, the result
     ret
