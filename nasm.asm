f7834587d1da927999879a1a39a37bcd199fbb83
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-15 21:39:51 +0200

    Update nasm.asm

--- nasm.asm
+++ nasm.asm
@@ -15,3 +15,5 @@ NAME:
 
 ; https://www.codewars.com/kumite/62d049e75bba551ca1badaed?sel=62d049e75bba551ca1badaed
 ; https://www.codewars.com/kumite/62d1471c5487c19e4e996064?sel=62d1471c5487c19e4e996064
+; pass functions
+; https://www.codewars.com/kata/545993ee52756d98ca0010e1/train/c
