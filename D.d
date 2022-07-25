1a00af1798b32c96f46a217bfcdf34e154d4ed04
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-25 08:32:12 +0200

    Create D.d

---
+++ D.d
@@ -0,0 +1,33 @@
+// shuffle
+// https://www.codewars.com/kumite/62dc17a2b86e07005c8b4ddb?sel=62dc17a2b86e07005c8b4ddb
+
+
+
+module solution;
+
+export int NAME(int n) 
+{
+    return 0;
+}
+
+
+
+
+module solution_test;
+
+import solution : NAME;
+import std.string : format;
+
+version(unittest) import fluent.asserts;
+
+void dotest(int n, int expected)
+{
+    NAME(n).should.equal(expected).because("n = %d".format(n));
+}
+
+@("Basic tests")
+unittest {
+    dotest({}, {});
+    
+    
+}
