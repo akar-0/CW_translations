525f6c5000f3325cb24018f64cae949848f27183
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-28 21:31:50 +0200

    Update D.d

--- D.d
+++ D.d
@@ -39,6 +39,11 @@
 // https://www.codewars.com/kumite/62e27fc15d64d4003db391b4?sel=62e27fc15d64d4003db391b4
 // only = iter once
 // https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/solutions/d
+// memoize remove pop dropBack reduce
+// https://www.codewars.com/kumite/62e2ce0729618a1a55bc1735?sel=62e2ce0729618a1a55bc1735
+
+
+
 
 module solution;
 
