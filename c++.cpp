c046e0a266810927f37c775ece94efb3baeefd79
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-28 12:58:01 +0200

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -3,3 +3,33 @@
 // https://docs.codewars.com/languages/cpp/igloo/stringizers
 
 // https://docs.codewars.com/languages/cpp/authoring/#custom-assertion-messages
+
+
+#include <ctime>
+#include <cstdlib>
+#include <string>  
+#include <sstream>  
+
+void dotest(int n, int expected) {
+  int actual = NAME(n);
+  std::stringstream ss;
+  ss << "Test failed with n = " << n << std::endl;
+  Assert::That(actual, Equals(expected), ExtraMessage(ss.str()));
+}
+
+Describe(tests)
+{
+    It(sample_tests)
+    {
+        dotest({}, {});
+    }
+  
+    It(random_tests)
+    {
+        srand(time(NULL));
+        for(int i = 0; i < 100; i++) {
+            int n = 6 + std::rand() % 149995;
+            dotest(n, referenceSolution(n));
+        }
+    }
+};
