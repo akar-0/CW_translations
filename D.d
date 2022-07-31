8deb5b6d0ccbee7a2fb0efdeeaf157d82a30e3ee
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-31 22:13:54 +0200

    Update D.d

--- D.d
+++ D.d
@@ -1,5 +1,5 @@
 import  std.range : chain, drop, enumerate, iota, only, repeat, zip;
-import  std.algorithm.iteration : each, filter, joiner, map;
+import  std.algorithm.iteration : each, filter, fold, joiner, map;
 import  std.array : array, join;
 import  std.conv : to;
 import  std.string : format, stripRight;
