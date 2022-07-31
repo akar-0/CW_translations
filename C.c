2c47812c4f96f74390137389191758e4a1850360
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-31 18:04:16 +0200

    Update C.c

--- C.c
+++ C.c
@@ -1,2 +1,6 @@
 // https://www.codewars.com/kumite/62961ed9fc63659ebf1808b1?sel=62961ed9fc63659ebf1808b1
 //https://www.codewars.com/kumite/62cf1289bf218d8f23bd1a40/edit
+
+
+// !!!!
+// https://www.codewars.com/kumite/62e6a62a323f44ce8b546057?sel=62e6a62a323f44ce8b546057
