35948e0f736f9b429c97672b7ae39311f601f493
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-02 19:28:09 +0200

    Update rust.rs

--- rust.rs
+++ rust.rs
@@ -47,7 +47,7 @@ mod tests {
 
 // return static string -> &'static str 
 // https://www.codewars.com/kumite/62912d0258d87b00168c9f52?sel=6291381158d87bd4458c8492
-
+// https://www.codewars.com/kumite/62e94ea330e5dc003065f04c?sel=62e94ea330e5dc003065f04c
 
 // advanced random
 // https://www.codewars.com/kumite/62a87515936a4a003d0b5ebd?sel=62a87515936a4a003d0b5ebd
