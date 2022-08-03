3a5997116a6e6a437ed540d46068bd21fce0d410
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-03 21:01:05 +0200

    Update D.d

--- D.d
+++ D.d
@@ -186,3 +186,5 @@ export  auto isSorted = (string s) => s.array.sort.equal(s);
 
     auto s = word.dup.representation.sort;
 
+// strings
+    auto allChars = iota('a', 'z' + 1).chain(iota('A', 'Z' + 1)).map!(c => cast(dchar) c).array;
