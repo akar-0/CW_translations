7db20a546757366a4aee07cd8c08a2c82229543a
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-04 01:40:59 +0200

    Update D.d

--- D.d
+++ D.d
@@ -190,3 +190,7 @@ export  auto isSorted = (string s) => s.array.sort.equal(s);
     auto allChars = iota('a', 'z' + 1).chain(iota('A', 'Z' + 1)).map!(c => cast(dchar) c).array;
 // !! slice strings, each...
 // https://www.codewars.com/kumite/62eab8a4879899004f82539c?sel=62eab8a4879899004f82539c
+
+
+// shuffleable dchar array
+                auto arr = uniform(minAn, maxAn, rnd).iota.map!(_ => allChars[uniform(0, $, rnd)]).map!(c => cast(dchar) c).array;
