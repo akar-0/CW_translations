841c3ec70e9e0466199a8f8dca2ddf1f10f05c6b
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-10 17:15:51 +0200

    Update C.c

--- C.c
+++ C.c
@@ -4,3 +4,16 @@
 
 // !!!!
 // https://www.codewars.com/kumite/62e6a62a323f44ce8b546057?sel=62e6a62a323f44ce8b546057
+
+
+
+#include <criterion/criterion.h>
+#include <math.h>
+
+extern double arrow_area(unsigned, unsigned);
+
+void dotest(unsigned a, unsigned b, double expected) {
+  double actual = arrow_area(a, b);
+  double err = fabs(actual - expected) / expected;
+  cr_assert(err < 1e-6, "With a = %u, b = %u\nExpected approximatively %f but got %f (error: %f)", a, b, expected, actual, err);
+}
